1f29f5bb5c7af22aafe798dd4e99bbf4f36ca006
Matthew Casperson <matthewcasperson@gmail.com>
2019-08-03 07:48:02 +1000

    Disable signing can also be a property

--- travis_publish.sh
+++ travis_publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [[ $TEST_SUITE = "{'browsers':['PhantomJS'],'runNegTests':true,'runSimpleTests':true}" ]]; then
 	ls -la
-	DisableSigning=false \
 	./gradlew \
+	-DisableSigning=false \
 	-Psigning.keyId=${KeyID} \
 	-Psigning.password=${KeyPassword} \
 	-Psigning.secretKeyRingFile=${KeyFilename} \
